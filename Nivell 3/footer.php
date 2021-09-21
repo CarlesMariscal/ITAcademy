832c0f9265ed706f4735736fc7db162418b5dabc
unknown <formacio@PC-1062667.formacio.local>
2021-09-21 17:24:16 +0200

    pagina terminada

---
+++ Nivell 3/footer.php
@@ -0,0 +1,6 @@
+    <div id="footer">
+            <center><i class="far fa-envelope"><a href="mailto:elcorreoquequieres@correo.com"> Contacto</a></i></center>
+            <div class="footer-content">
+                <p align="center">Copyright &copy;2021 designed by <span>Carles Mariscal</span></p>
+            </div>
+    </div>
---
+++ Nivell 3/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include 'header.php'; ?>
+<body>
+    <div id="contenedor">
+        <div id="header">
+            <div id="logo"> 
+               <img src="Imagenes/LOGO-logo/cover.png" alt="logo" width="100%" height="100%"> 
+            </div>
+            <div id="social">
+                <div id="facebook" class="rrss">
+                    <i class="fab fa-facebook"></i>
+                </div>
+                <div id="twitter" class="rrss">
+                    <i class="fab fa-twitter"></i>
+                </div>
+                <div id="instagram" class="rrss">
+                    <i class="fab fa-instagram"></i>
+                </div>
+            </div>
+        </div>
+        <div id="nav">
+            <div id="box1" class="navegacion"> <h3 align="center">Inicio</h3></div>
+            <div id="box2" class="navegacion"> <h3 align="center">Informacion</h3></div>
+            <div id="box3" class="navegacion"> <h3 align="center">Productos</h3></div>
+            <div id="box4" class="navegacion"> <h3 align="center">Multimedia</h3></div>
+            <div id="box5" class="navegacion"> <h3 align="center">Blog</h3></div>
+        </div>
+        <div id="section1">
+            <div id="box6" class="sec1">
+                <a href="https://www.dqcolor.es/2484-thickbox_default/amarillo-pastel.jpg">
+                    <img src="Imagenes/LOGO-logo/amarillo-pastel.jpg" alt="amarillo" width="100%" height="100%">
+                </a>    
+            </div>
+            <div id="box7" class="sec1">
+                <a href="https://www.laparisina.mx/media/catalog/product/cache/c687aa7517cf01e65c009f6943c2b1e9/0/3/0321l50_01.jpg">
+                    <img src="Imagenes/LOGO-logo/descarga.jpg" alt="rosa" width="100%" height="100%">
+                </a>       
+            </div>
+            <div id="box8" class="sec1">
+                <a href="https://www.kilotela.com/wp-content/uploads/2017/09/tela-sabana-lisa-280-ancho-verde.jpg">
+                    <img src="Imagenes/LOGO-logo/tela-sabana-lisa-280-ancho-verde.jpg" alt="verde" width="100%" height="100%">
+                </a>   
+            </div>
+        </div>
+        <div id="section2">
+            <aside id="left">
+                <img src="Imagenes/LOGO-logo/anuncio l.jpg" alt="" width="100%" height="100%">
+            </aside>
+            <article>
+                <center>
+                    <img style="border: 5px solid; color:black;" src="Imagenes/LOGO-logo/article.jpg" alt="Lil Uzi Vert, con su diamante rosa implantado en la frente" width="50%" height="50%">
+                </center>
+                <h2>Arrancan el diamante que un rapero llevaba incrustado en la frente en pleno concierto</h2>
+                <p align="justify">
+                    La piedra preciosa está valorada en unos 20 millones de euros, aunque la buena noticia es que el cantante pudo conservar su diamante rosa 
+                </p>
+                <a href="https://www.elconfidencial.com/cultura/2021-09-10/lil-uzi-vert-diamante-concierto-rapero_3284430/">
+                    <center><input type="submit" class="boton" value="Ver mas"></center>
+                </a>
+            </article>
+            <aside id="right">
+                <img src="Imagenes/LOGO-logo/anuncio r.jpg" alt="" width="100%" height="100%">
+            </aside>
+        </div>
+        <?php include 'footer.php'; ?>
+    </div>
+</body>
+</html>
